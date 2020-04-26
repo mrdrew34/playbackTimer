433c29ff812d7cdeae2eceebabb05fda58d22b8e
Andrew <64363929+mrdrew34@users.noreply.github.com>
2020-04-26 11:36:27 -0700

    Cleanup and Add Comments

--- timeDelta.py
+++ timeDelta.py
@@ -3,48 +3,46 @@
 
 reload =  True
 while reload == True:
-    # Get the times from the user
+    #### User Inputs ####
     hours = abs(float(input('How many hours is the video? ')))
     minutes = abs(int(input('How many minutes is the video? ')))
     seconds = abs(int(input('How many seconds is the video? ')))
     speed = abs(float(input('What is the new speed? ')))
 
+    #### Time Conversions to Hours ####
     # convert minutes to hours
-    minToHr = minutes / 60.0
-    
+    minToHr = minutes / 60.0    
     # convert seconds to hours
     secToHr = seconds / 3600.0
-    
+
+    #### Time Totals ####    
     # add the total hours
-    totalHr = hours + minToHr + secToHr
-    
+    totalHr = hours + minToHr + secToHr    
     # calculate the relative speed (1 / speed change)
-    relativeSpeed = float(1/speed)
-    
+    relativeSpeed = float(1/speed)    
     # calculate change in hours due to speed change
-    newHr = totalHr * relativeSpeed
-    
+    newHr = totalHr * relativeSpeed    
     # find minutes remaining
-    hrToMin = (newHr - int(newHr)) * 60
-    
+    hrToMin = (newHr - int(newHr)) * 60    
     # find seconds remaining
     minToSec = (hrToMin - int(hrToMin)) * 60
-    
-    # Calculate the amount of time saved (work in progress)
+        
+    #### Calculate changes in time####    
+    # Calculate the amount of time saved
     DiffHr = totalHr - newHr
-    print(DiffHr)
+    # Calculate change in hours
     deltaHr = (DiffHr - int(DiffHr)) / 60
-    print(deltaHr)
+    # Calculate change in minutes
     deltaHrToMin = (deltaHr - int(deltaHr)) * 3600
-    print(deltaHrToMin)
+    # Calculate change in seconds
     deltaMinToSec = (deltaHrToMin - int(deltaHrToMin)) * 60
-    print(deltaMinToSec)
-
+    
+    #### Print Calculation Outputs ####
     print('The total time now is',int(newHr),'hour(s)',int(hrToMin),'minute(s)',int(round(minToSec)),'second(s).')
     print('You have saved',int(DiffHr),'hour(s)',int(deltaHrToMin),'minute(s)',int(round(deltaMinToSec)),'second(s).')
     calcAnother = input("Would you like to run calculator again? (yes/no)").casefold()
 
-
+    #### Recalculate Decision ####
     if calcAnother == 'yes': reload = True
     elif calcAnother == 'y': reload = True
     elif calcAnother != 'yes': break
