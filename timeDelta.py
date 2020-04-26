441849056b1a7f3d683d34c942238a07c0649197
Andrew <64363929+mrdrew34@users.noreply.github.com>
2020-04-26 10:02:43 -0700

    Case-insensitive answer and input absolute

--- timeDelta.py
+++ timeDelta.py
@@ -3,11 +3,10 @@
 
 reload =  True
 while reload == True:
-
-    hours = float(input('How many hours is the video? '))
-    minutes = int(input('How many minutes is the video? '))
-    seconds = int(input('How many seconds is the video? '))
-    speed = float(input('What is the new speed? '))
+    hours = abs(float(input('How many hours is the video? ')))
+    minutes = abs(int(input('How many minutes is the video? ')))
+    seconds = abs(int(input('How many seconds is the video? ')))
+    speed = abs(float(input('What is the new speed? ')))
 
     # hrToSec = hours * 3600
     minToHr = minutes / 60.0
@@ -29,9 +28,9 @@ while reload == True:
 
     print('The total time now is',int(newHr),'hour(s)',int(hrToMin),'minute(s)',int(round(minToSec)),'second(s).')
     
-    calcAnother = input("Would you like to run calculator again? (yes/no)")
-
-    if calcAnother == "yes": reload = True
-    elif calcAnother == "no": break
+    calcAnother = input("Would you like to run calculator again? (yes/no)").casefold()
 
 
+    if calcAnother == 'yes': reload = True
+    elif calcAnother == 'y': reload = True
+    elif calcAnother != 'yes': break
